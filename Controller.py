e44f83a6d87ed39ffe92fe85efe25ec4d4d1697e
Lewis Robbins <lewis.robbins2@gmail.com>
2022-03-26 23:02:17 +0000

    add print logic

--- Controller.py
+++ Controller.py
@@ -1,21 +1,35 @@
 from collections import defaultdict
-from sys import exit
+import sys
+from colors import color
 
 class Controller:
     def __init__(self, filters):
         self.filters = filters
 
     def run(self):
-        self.results = defaultdict(int)
+        self.results = defaultdict(bool)
         for f_filter in self.filters:
             if f_filter.match():
-                d[str(f_filter)] = 1
+                self.results[str(f_filter)] = True
 
         if any(i > 0 for i in self.results.values()):
             print('Rules matched')
-            for key, value in self.results:
-                print(f"Rule {key} has {value}")
+            for key, value in self.results.items():
+                if value:
+                    print(f"{color.bcolors.FAIL}Rule {key} has {value}{color.bcolors.ENDC}")
+                else:
+                    print(f"{color.bcolors.OKCYAN}Rule {key} has {value}{color.bcolors.ENDC}")
             sys.exit(-1)
         else:
             print('No rules matched')
 
+class EC2():
+    def __repr__(self):
+        return f"Rule EC2"
+    def __init__(self):
+        pass
+    def match(self):
+        return True
+f = [EC2()]
+c = Controller(f)
+c.run()
